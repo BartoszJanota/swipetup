e8aa061966cc0be9b138456f826453e185436694
PrzemekBurczyk <przemek.burczyk@gmail.com>
2014-12-06 20:22:28 +0100

    Made resources loading for timeline. Timeline now is displayed properly.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,23 @@
-jQuery(document).ready(function($){
-	var $timeline_block = $('.cd-timeline-block');
+jQuery(document).ready(function ($) {
+    var $timeline_block = $('.cd-timeline-block');
 
-	//hide timeline blocks which are outside the viewport
-	//animate timeline block which are inside the viewport
-	$timeline_block.each(function(){
-		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
-			$(this).find('.cd-timeline-bubble, .cd-timeline-content').addClass('is-hidden');
-		} else {
-			$(this).find('.cd-timeline-bubble, .cd-timeline-content').addClass('bounce-in');
-		}
-	});
+    //hide timeline blocks which are outside the viewport
+    //animate timeline block which are inside the viewport
+    $timeline_block.each(function () {
+        if ($(this).offset().top > $(window).scrollTop() + $(window).height() * 0.5) {
+            $(this).find('.cd-timeline-bubble, .cd-timeline-content').addClass('is-hidden');
+        } else {
+            $(this).find('.cd-timeline-bubble, .cd-timeline-content').addClass('bounce-in');
+        }
+    });
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		$timeline_block.each(function(){
-			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-bubble').hasClass('is-hidden') ) {
-				$(this).find('.cd-timeline-bubble, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-			}
-		});
-	});
+    //on scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function () {
+        $timeline_block.each(function () {
+            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.5 && $(this).find('.cd-timeline-bubble').hasClass('is-hidden')) {
+                console.log('bounce in');
+                $(this).find('.cd-timeline-bubble, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+            }
+        });
+    });
 });
