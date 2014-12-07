b8ffc3b90c953365ac6ed9526c2d195d62d41dd4
PrzemekBurczyk <przemek.burczyk@gmail.com>
2014-12-08 00:15:59 +0100

    Implemented routing to /home if logged already.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,15 +6,22 @@ import play.api._
 import play.api.mvc._
 import util.OAuth2
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 object Application extends Controller {
 
-  def signin = Action { implicit request =>
-    val callbackURL = util.routes.OAuth2.callback(None, None).absoluteURL()
-    val oauth2 = new OAuth2(Play.current)
-    val state = UUID.randomUUID().toString()
-    val accessScope = "basic" //Access to Meetup group info, Everywhere API, creating and editing Events and RSVP's, posting photos
-  val redirectMeetupUrl = oauth2.getAuthorizationUrl(callbackURL, accessScope, state)
-    Ok(views.html.signin("Your new application is ready.", redirectMeetupUrl)).withSession("oauth-state" -> state)
+  def signin = Action.async { implicit request =>
+    request.session.get("logged-name").map { loggedUser =>
+      Future(Redirect(routes.Home.init()))
+    }.getOrElse {
+      val callbackURL = util.routes.OAuth2.callback(None, None).absoluteURL()
+      val oauth2 = new OAuth2(Play.current)
+      val state = UUID.randomUUID().toString()
+      val accessScope = "basic" //Access to Meetup group info, Everywhere API, creating and editing Events and RSVP's, posting photos
+      val redirectMeetupUrl = oauth2.getAuthorizationUrl(callbackURL, accessScope, state)
+      Future(Ok(views.html.signin("Your new application is ready.", redirectMeetupUrl)).withSession("oauth-state" -> state))
+    }
   }
 
 }
